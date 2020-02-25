c26b9632d110b6651b64e2d02eb05a6b4d4150c7
joshuamoore533 <joshuamoore533@gmail.com>
2020-02-24 21:36:30 -0600

    new file:   Clicker_Game 	new file:   Clicker_Game.cpp 	new file:   Clicker_Game_Header.h

---
+++ Clicker_Game.cpp
@@ -0,0 +1,48 @@
+
+//clicker game
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Clicker_Game_Header.h"
+
+using namespace std;
+
+
+void hold_screen();
+
+int main(){
+int user_selec;
+	string str;
+str.assign(22,'-');
+cout<<"The Life Clicker Game!";
+cout<<endl;
+cout<<str;
+cout<<endl;
+
+
+cout<<"Start Preschool (1)";
+cout<<endl;
+
+
+
+
+cout<<"User Input: ";cin>>user_selec;
+
+switch(user_selec){
+
+case 1:cout<<"You chose to start preschool";break;
+
+
+default:break;
+
+
+
+
+
+	return 0;
+};
+
+
+};
+
