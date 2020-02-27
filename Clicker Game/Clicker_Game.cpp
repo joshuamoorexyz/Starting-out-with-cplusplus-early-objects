60c2a5b9f0813b60e84aa8af2fc4af04cc309a0a
joshuamoore533 <joshuamoore533@gmail.com>
2020-02-27 12:45:44 -0600

    deleted:    Clicker Game/Clicker_Game 	modified:   Clicker Game/Clicker_Game.cpp 	new file:   Clicker Game/a.out

--- Clicker Game/Clicker_Game.cpp
+++ Clicker Game/Clicker_Game.cpp
@@ -7,51 +7,60 @@
 #include <stdio.h>
 
 
-using namespace std;
+
+#include <SDL2/SDL.h>
+
+int main (int argc, char** argv)
+{
+    SDL_Window* window = NULL;
+    window = SDL_CreateWindow
+    (
+        "Life Clicker!", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN
+    );
+
+    // Setup renderer
+    SDL_Renderer* renderer = NULL;
+    renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Set render color to red ( background will be rendered in this color )
+    SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+
+    // Clear winow
+    SDL_RenderClear( renderer );
+
+    // Creat a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
+    SDL_Rect r;
+    r.x = 50;
+    r.y = 50;
+    r.w = 50;
+    r.h = 50;
+
+    // Set render color to blue ( rect will be rendered in this color )
+    SDL_SetRenderDrawColor( renderer, 0, 0, 255, 120 );
+
+    // Render rect
+    SDL_RenderFillRect( renderer, &r );
+
+    // Render the rect to the screen
+    SDL_RenderPresent(renderer);
+
+    // Wait for 5 sec
+    SDL_Delay( 5000 );
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
 
 
-void hold_screen();
 
-int main(){
-char user_selec;
-	string str;
-double total_money=0.0;
-str.assign(22,'-');
-cout<<"The Life Clicker Game!";
-cout<<endl;
-cout<<str;
-cout<<endl;
+ 
 
+   
 
-cout<<endl;
-
-do{
-cout<<"Total Money: $"<<total_money;
-cout<<endl;
-
-cout<<"Start Preschool (a)";
-cout<<endl;
-
-
-cout<<"User Input: ";cin>>user_selec;
-
-
-switch(user_selec){
-
-case 'a':cout<<fixed<<showpoint;
-cout<<setprecision(2);
-total_money=total_money +5.00;cout<<endl;
-
-break;
-
-default :cout<<"That is not a valid option";
-cout<<endl;
-break;
-
-
-	
-};
-}while(user_selec!='z');
-return 0;
-};
 
