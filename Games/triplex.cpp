aa9e8c5dc094a3e2085541afc21c9fd8546154a0
joshuamoore533 <40312633+joshuamoore533@users.noreply.github.com>
2020-03-08 19:42:57 -0500

    learning game dev

---
+++ Games/triplex.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+cout<<"Hello World"<<endl;
+int user_input;
+cout<<"How many cookies do you have?";
+cout<<endl;
+cout<<"# of cookies: ";cin>>user_input;
+cout<<endl;
+cout<<"You have "<<user_input<<" cookies"<<endl;
+cout<<endl;
+
+
+    return 0;
+}
